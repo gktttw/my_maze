0b75840924297dfaf97e925ce60593b55ee501e8
gktttw <gktttwsam@gmail.com>
2024-07-24 14:40:17 +0800

    chore(annotate): add gem to generate model schema docs

--- app/models/maze.rb
+++ app/models/maze.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: mazes
+#
+#  id         :bigint           not null, primary key
+#  size       :integer          not null
+#  grid       :json             not null
+#  solution   :json             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Maze < ApplicationRecord
   validates :size, presence: true
   validates :grid, presence: true
--- test/models/maze_test.rb
+++ test/models/maze_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: mazes
+#
+#  id         :bigint           not null, primary key
+#  size       :integer          not null
+#  grid       :json             not null
+#  solution   :json             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require "test_helper"
 
 class MazeTest < ActiveSupport::TestCase
